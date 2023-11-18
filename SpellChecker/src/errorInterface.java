a6d961e2f31f6ed439f02d1d267369fdd3d09850
Jwalant Patel <142465606+Jawlt@users.noreply.github.com>
2023-11-18 17:51:04 -0500

    Update errorInterface.java

---
+++ SpellChecker/src/errorInterface.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class errorInterface extends userDictionary {
+    private String correctWord;
+    private String originalWord;
+    String userDocument;
+    List<String> correctionList = new ArrayList<String>();
+    List<String> errorSummaryBlock = new ArrayList<String>();
+    Dictionary errorCorrectionMetrics = new Hashtable();
+  
+    public void showPossibleCorrections(List<String> correctionList){
+
+    }
+
+    public String applyCorrection(String orignalWord, String correctWord){
+        return
+    }
+
+    public void ignoreError(String originalWord){
+
+    }
+
+    public void addToDictionary(String originalWord,List<String> userDictionary ){
+
+    }
+
+    public String viewErrorSummary(){
+
+    }
+
+    public String viewCorrectionMetrics(){
+
+    }
+}
