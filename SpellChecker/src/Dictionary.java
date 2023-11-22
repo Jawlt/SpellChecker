9a5da390c989e5130c73044cba4a2af8e9aed6d1
ojashunjan <115956134+ojashunjan@users.noreply.github.com>
2023-11-22 14:22:29 -0500

    Update Dictionary.java

--- SpellChecker/src/Dictionary.java
+++ SpellChecker/src/Dictionary.java
@@ -3,136 +3,167 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.IOException;
+import java.io.IOException;//packages to use in class
 
-public class Dictionary {
+public class Dictionary {//creation of class
 
-    private List<String> wordList;
+    private List<String> wordList;//private instance variable for wordList
 
-    private List<String> dictionary;
+    private List<String> dictionary;//private instance variable for dictionary
 
-    private List<String> userDictionary;
+    private List<String> userDictionary;//private instance variable for user dictionary
 
-    private List<String> combinedDictionary;
+    private List<String> combinedDictionary;//private instance variable for combined dictionary
 
-    public Dictionary() {
+    /**
+     * Constructor method that sets instance variables as new array lists
+     */
+    public Dictionary() {//create constructor method
 
-        this.wordList = new ArrayList<>();
-        this.dictionary = new ArrayList<>();
-        this.userDictionary = new ArrayList<>();
-        this.combinedDictionary = new ArrayList<>();
+        this.wordList = new ArrayList<>();//create word list
+        this.dictionary = new ArrayList<>();//create dictionary
+        this.userDictionary = new ArrayList<>();//create user dictionary
+        this.combinedDictionary = new ArrayList<>();//create combined dictionary
     }
 
-    public List<String> loadDictionary(String nameofDictionary) {
+    /**
+     * This method loads the dictionary given the name of the dictionary as the parameter (filepath). It returns this dictionary.
+     * @param nameofDictionary
+     * @return
+     */
+    public List<String> loadDictionary(String nameofDictionary) {//creation of this method
 
-        List<String> wordsToadd = new ArrayList<>();
+        List<String> wordsToadd = new ArrayList<>();//create new array list of words to add to the return dictionary from the loaded dictionary.
 
         try {
-            BufferedReader dictionaryReader = new BufferedReader(new FileReader(nameofDictionary));
+            BufferedReader dictionaryReader = new BufferedReader(new FileReader(nameofDictionary));//bufferedreader to read the dictionary given the file of the dict
 
-            String dictionaryLine;
+            String dictionaryLine;//string reprresenting the word/line of the dictionary
 
             try {
 
-                while ((dictionaryLine = dictionaryReader.readLine()) != null) {
+                while ((dictionaryLine = dictionaryReader.readLine()) != null) {//while loop for is the line of the dictionary is not null (has a word there)
 
-                    wordsToadd.add(dictionaryLine);
+                    wordsToadd.add(dictionaryLine);//add this word to the created array list
 
 
                 }
             } 
-            catch (IOException e) {
+            catch (IOException e) {//catches exception if the dictionary is not found
                 e.printStackTrace();
             }
 
         } 
         
-        catch (FileNotFoundException e) {
+        catch (FileNotFoundException e) {//catches exception if the file for the dictionary is not found
 
-            System.out.println("Error, dictionary is not found.");
+            System.out.println("Error, dictionary is not found.");//prints error message
         }
 
-        return wordsToadd;
+        return wordsToadd;//return the loaded dictionary
 
     }
 
-    public List<String> loadUserDictionary(String nameofUserDictionary) {
+    /**
+     * This method loads the user dictionary given the name of the user dictionary as the parameter (filepath). It returns this dictionary.
+     * @param nameofUserDictionary
+     * @return
+     */
+    public List<String> loadUserDictionary(String nameofUserDictionary) {//creation of this method
 
-        List<String> wordsToadd = new ArrayList<>();
+        List<String> wordsToadd = new ArrayList<>();//create new array list of words to add to the return user dictionary from the loaded user dictionary.
 
         try {
-            BufferedReader dictionaryReader = new BufferedReader(new FileReader(nameofUserDictionary));
+            BufferedReader dictionaryReader = new BufferedReader(new FileReader(nameofUserDictionary)); //bufferedreader to read the user dictionary given the file of the dict
 
-            String dictionaryLine;
+            String dictionaryLine;//string reprresenting the word/line of the user dictionary
 
             try {
 
-                while ((dictionaryLine = dictionaryReader.readLine()) != null) {
+                while ((dictionaryLine = dictionaryReader.readLine()) != null) {//while loop for is the line of the user dictionary is not null (has a word there)
 
-                    wordsToadd.add(dictionaryLine);
+                    wordsToadd.add(dictionaryLine);//add this word to the created array list
 
 
                 }
             } 
-            catch (IOException e) {
+            catch (IOException e) {//catches exception if the user dictionary is not found
                 e.printStackTrace();
             }
 
         } 
         
-        catch (FileNotFoundException e) {
+        catch (FileNotFoundException e) {//catches exception if the file for the dictionary is not found
 
-            System.out.println("Error, dictionary is not found.");
+            System.out.println("Error, dictionary is not found.");//prints error message
         }
 
-        return wordsToadd;
+        return wordsToadd;//return the loaded dictionary
     }
 
-    public List<String> combineDictionary(List<String> wordList, List<String> userDictionary) {
+    /**
+     * This method takes in the wordList and user dictionary and combines them to create one, solidified dictionary and returns it
+     * @param wordList
+     * @param userDictionary
+     * @return
+     */
+    public List<String> combineDictionary(List<String> wordList, List<String> userDictionary) {//create the method
 
-        this.combinedDictionary.clear();
+        this.combinedDictionary.clear();//clear the combined dictionary
 
-        this.combinedDictionary.addAll(wordList);
+        this.combinedDictionary.addAll(wordList);//add all the words of the word list to the combined dictionary
 
-        this.combinedDictionary.addAll(userDictionary);
+        this.combinedDictionary.addAll(userDictionary);//add all the words of the user dictionary to the combined dictionary
 
-        return this.combinedDictionary;
+        return this.combinedDictionary;//return the combined dictionary
     }
 
-    public List<String> addWord(String word, List<String> wordList) {
+    /**
+     * This method adds a word to the wordList and returns it updated
+     * @param word
+     * @param wordList
+     * @return
+     */
+    public List<String> addWord(String word, List<String> wordList) {//Creation of the method
 
-        wordList.add(word);
+        wordList.add(word);//add the word to the word list
 
-        return wordList;
-
-    }
-
-    public List<String> removeWord(String word, List<String> wordList) {
-
-        wordList.remove(word);
-
-        return wordList;
+        return wordList;//returns the word list
 
     }
 
-    public List<String> getwordList() {
+    /**
+     * This method removes a word from the wordList and returns it updated
+     * @param word
+     * @param wordList
+     * @return
+     */
+    public List<String> removeWord(String word, List<String> wordList) {//creation of the method
 
-        return this.wordList;
+        wordList.remove(word);//remove the word from the word list
+
+        return wordList;//returns the word list
+
     }
 
-    public List<String> getDictionary() {
+    public List<String> getwordList() {//getter function for wordList
 
-        return this.dictionary;
+        return this.wordList;//return wordList
     }
 
-    public List<String> getUserDictionary() {
+    public List<String> getDictionary() {//getter function for dictionary
 
-        return this.userDictionary;
+        return this.dictionary;//return dictionary
     }
 
-    public List<String> getCombinedDictionary() {
+    public List<String> getUserDictionary() {//getter function for user dictionary
 
-        return this.combinedDictionary;
+        return this.userDictionary;//return user dictionary
+    }
+
+    public List<String> getCombinedDictionary() {//getter function for combined dictionary
+
+        return this.combinedDictionary;//returns combined dictionary
     }
 
 
