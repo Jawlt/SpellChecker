cb5e2dde1ca81a94536e4282d92ca6bfc53acc09
Ojas Singh Hunjan <115956134+ojashunjan@users.noreply.github.com>
2023-11-30 01:43:38 -0500

    Create IntegrationTest.java

---
+++ SpellChecker/src/IntegrationTest.java
@@ -0,0 +1,135 @@
+import org.junit.jupiter.api.Test;
+
+//import java.util.Dictionary;
+import java.util.Hashtable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntegrationTest {
+
+    @Test
+    public void OutputofCombiningUserandTestDictionary() {
+
+        checkSpelling corrections = new checkSpelling();
+
+        Dictionary dict = new Dictionary();
+
+        Hashtable<String, String> testDict = dict.getDictionary();
+
+        Hashtable<String, String> userDict = dict.getUserDictionary();
+
+        Hashtable<String, String> combineddict = new Hashtable<>();
+
+        userDict.put("test", "test");
+
+        testDict.put("program", "program");
+
+        dict.combineDictionary();
+
+        combineddict.put("test", "test");
+
+        combineddict.put("program", "program");
+
+        assertEquals(combineddict, dict.getDictionary());
+
+    }
+
+    @Test
+    public void fixErrorinuserDictbeforeCombining() {
+
+        checkSpelling corrections = new checkSpelling();
+
+        Dictionary dict = new Dictionary();
+
+        Hashtable<String, String> testDict = dict.getDictionary();
+
+        Hashtable<String, String> userDict = dict.getUserDictionary();
+
+        Hashtable<String, String> combineddict = new Hashtable<>();
+
+        userDict.put("tes", "test");
+
+        testDict.put("program", "program");
+
+        corrections.setDictionary(userDict);
+
+        corrections.getOmission("tes");
+
+        dict.combineDictionary();
+
+        combineddict.put("test", "test");
+
+        combineddict.put("program", "program");
+
+        assertEquals(combineddict, dict.getDictionary());
+
+    }
+
+    @Test
+    public void fixErrorintestDictbeforeCombining() {
+
+        checkSpelling corrections = new checkSpelling();
+
+        Dictionary dict = new Dictionary();
+
+        Hashtable<String, String> testDict = dict.getDictionary();
+
+        Hashtable<String, String> userDict = dict.getUserDictionary();
+
+        Hashtable<String, String> combineddict = new Hashtable<>();
+
+        userDict.put("test", "test");
+
+        testDict.put("pprogram", "program");
+
+        corrections.setDictionary(userDict);
+
+        corrections.getInsertion("pprogram");
+
+        dict.combineDictionary();
+
+        combineddict.put("test", "test");
+
+        combineddict.put("program", "program");
+
+        assertEquals(combineddict, dict.getDictionary());
+
+    }
+
+    @Test
+    public void addwordafterDictionaryisCombined() {
+
+        Dictionary dict = new Dictionary();
+        Hashtable<String, String> testDict = dict.getDictionary();
+        Hashtable<String, String> userDict = dict.getUserDictionary();
+        Hashtable<String, String> combineddict = new Hashtable<>();
+        testDict.put("test", "test");
+        userDict.put("the", "the");
+        combineddict.put("test", "test");
+        combineddict.put("the", "the");
+        combineddict.put("program", "program");
+        dict.combineDictionary();
+        dict.getDictionary().put("program", "program");
+        assertEquals(combineddict, dict.getDictionary());
+
+    }
+
+    @Test
+    public void removewordafterDictionaryisCombined() {
+
+        Dictionary dict = new Dictionary();
+        Hashtable<String, String> testDict = dict.getDictionary();
+        Hashtable<String, String> userDict = dict.getUserDictionary();
+        Hashtable<String, String> combineddict = new Hashtable<>();
+        testDict.put("test", "test");
+        testDict.put("the", "the");
+        userDict.put("program", "program");
+        combineddict.put("test", "test");
+        combineddict.put("the", "the");
+        dict.combineDictionary();
+        dict.getDictionary().remove("program", "program");
+        assertEquals(combineddict, dict.getDictionary());
+
+    }
+
+}
