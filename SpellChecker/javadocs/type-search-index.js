9074068cb4dda9db00c779714d8a692ee5ca0e87
Ojas Singh Hunjan <115956134+ojashunjan@users.noreply.github.com>
2023-11-30 21:45:12 -0500

    uploaded all java documentation

---
+++ SpellChecker/javadocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"checkSpelling"},{"p":"<Unnamed>","l":"Dictionary"},{"p":"<Unnamed>","l":"GUI"},{"p":"<Unnamed>","l":"TextFileDropTargetListener"},{"p":"<Unnamed>","l":"userDictionary"}];updateSearchResults();
